27c3d814d28d64b2b22c648dcb9b9ca4ed778e37
Lex Neva <lneva@heroku.com>
2016-08-29 15:10:14 -0400

    add TimelineEntry model

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -2,4 +2,5 @@ class Incident < ActiveRecord::Base
   has_many :retrospectives
   has_many :remediations, through: :retrospectives
   belongs_to :category
+  has_many :timeline_entries
 end
---
+++ spec/factories/timeline_entries.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :timeline_entry do
+    user nil
+    timestamp "2016-08-29 13:53:01"
+    message "MyText"
+    incident nil
+  end
+end
