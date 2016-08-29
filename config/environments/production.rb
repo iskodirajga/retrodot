51d74164ca8a6f8068944c0e746b5ca8afe9b3a4
Lex Neva <lexelby@users.noreply.github.com>
2016-08-29 08:59:07 -0400

    remove require of ssl-enforcer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  require 'rack/ssl-enforcer'
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
