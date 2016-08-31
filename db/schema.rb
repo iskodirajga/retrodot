2eb2b71b706312923f353680b7a94f3fa2164644
Lex Neva <lneva@heroku.com>
2016-08-31 10:01:30 -0400

    add start and end of chat to incident

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160830203407) do
+ActiveRecord::Schema.define(version: 20160831140017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20160830203407) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.datetime "chat_start"
+    t.datetime "chat_end"
     t.index ["category_id"], name: "index_incidents_on_category_id", using: :btree
   end
 
