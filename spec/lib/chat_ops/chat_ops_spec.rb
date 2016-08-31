82ec2bd8304133ca348668e29633a091660fb9d4
JuanitoFatas <katehuang0320@gmail.com>
2016-09-01 02:46:55 +0800

    Improves ChatOps specs
    - use `&:regex`
    
      Because [it is faster](https://github.com/JuanitoFatas/fast-ruby#block-vs-symbolto_proc-code)
    
    - Delete spec/chat_ops/chat_ops_spec.rb
      move spec to a location that matched the class definition
    
      `lib/chat_ops/chat_ops.rb` => `spec/lib/chat_ops/chat_ops.rb`
    
      * Because some users may have editor plugin that automatically goes to the
        spec if the location matches, this help them find the correct spec
      * Also this consistency helps us find the right spec when our codebase grows
    
    - (hopefully) improves specs
      * describe the method we are testing
      * decouples specs setup, no shared data => easily changed in the future
      * each spec follow this style:
        [setup exercise verify (teardown) style](https://robots.thoughtbot.com/four-phase-test)

---
+++ spec/lib/chat_ops/chat_ops_spec.rb
@@ -0,0 +1,70 @@
+RSpec.describe ChatOps do
+  before do
+    ChatOps.class_variable_set :@@commands, []
+  end
+
+  describe '.register' do
+    it 'adds class to commands class variable' do
+      Foo = Class.new
+
+      ChatOps.register Foo
+
+      expect(ChatOps.commands).to include Foo
+    end
+  end
+
+  describe '.commands' do
+    it 'returns an array object' do
+      expect(ChatOps.commands).to be_an Array
+    end
+
+    it 'registers when some class inherits from ChatOpsCommand' do
+      TestCommand1 = Class.new(ChatOpsCommand)
+      TestCommand2 = Class.new(ChatOpsCommand)
+
+      expect(ChatOps.commands).to include TestCommand1, TestCommand2
+    end
+  end
+
+  describe '.matcher' do
+    let(:cmd_1_class) { Class.new(ChatOpsCommand) }
+    let(:cmd_2_class) { Class.new(ChatOpsCommand) }
+
+    it 'builds a regex from defined commands' do
+      cmd_1_class.class_eval { match /test_regex1_[0-9]+/ }
+      cmd_2_class.class_eval { match /test_regex2_[a-z]+/ }
+
+      expect(ChatOps.matcher).to match 'test_regex1_0123'
+      expect(ChatOps.matcher).to match 'test_regex2_abcdef'
+    end
+  end
+
+  describe '.process' do
+    let(:cmd_1_class) { Class.new(ChatOpsCommand) }
+    let(:cmd_2_class) { Class.new(ChatOpsCommand) }
+    let!(:cmd_1_instance) { cmd_1_class.new }
+    let!(:cmd_2_instance) { cmd_2_class.new }
+
+    before do
+      cmd_1_class.class_eval { match /test_regex1_[0-9]+/ }
+      cmd_2_class.class_eval { match /test_regex2_[a-z]+/ }
+      allow(cmd_1_class).to receive(:new) { cmd_1_instance }
+      allow(cmd_2_class).to receive(:new) { cmd_2_instance }
+    end
+
+    it 'calls process on an instance of each defined command' do
+      expect(cmd_1_instance).to receive(:process)
+      expect(cmd_2_instance).to receive(:process)
+
+      ChatOps.process('user', 'message')
+    end
+
+    it 'calls run only on the command that matched' do
+      cmd_2_class.class_eval { def run(user, result); 'Command2.run'; end }
+
+      result = ChatOps.process('user', 'test_regex2_abcdef')
+
+      expect(result).to eq 'Command2.run'
+    end
+  end
+end
