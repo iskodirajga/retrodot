1e43c5cef65ea48a01d05877210963d4c0222b66
Lex Neva <lneva@heroku.com>
2016-09-02 10:24:42 -0400

    restore mistakenly commented test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe User do
-  #it { should have_many(:timeline_entries) }
+  it { should have_many(:timeline_entries) }
   it "should only allow valid handles" do
     expect(build(:user, handle: "abcd123abc")).to be_valid
     expect(build(:user, handle: "abcD123abc")).not_to be_valid
