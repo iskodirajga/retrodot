973119c9d4b52c54e5ec4a6b16f1b8e1cbfe959a
Lex Neva <lneva@heroku.com>
2016-09-14 14:20:03 -0400

    add logging to Mediators::User::Sync

--- app/support/mediators/base.rb
+++ app/support/mediators/base.rb
@@ -4,7 +4,12 @@ module Mediators
     include Loggerator
 
     def self.run(options={})
-      new(options).call
+      log_context(mediator: self.name) do
+        log action: "call"
+        result = new(options).call
+        log action: "end"
+        result
+      end
     end
 
     def initialize(options={})
