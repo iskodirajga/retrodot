9ba7c3d45aa2dfd47760769c501e27ff91df65eb
Joshua Tobin <joshuatobin@gmail.com>
2016-09-15 20:46:17 -0400

    Add chatops generator

---
+++ lib/generators/chatops/chatops_generator.rb
@@ -0,0 +1,37 @@
+class ChatopsGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+
+  def copy_file
+    create_file "lib/chat_ops/commands/#{file_name}_command.rb", <<-FILE
+module ChatOps::Commands
+  class #{class_name}Command < ChatOps::ChatOpsCommand
+    match //
+    help_message ""
+  end
+
+  def run(user, match)
+    # ChatOps.message()
+  end
+end
+    FILE
+  end
+
+  def copy_spec
+    create_file "spec/lib/chat_ops/commands/#{file_name}_command_spec.rb", <<-FILE
+RSpec.describe ChatOps::Commands::AddResponderCommand do
+  include ChatOpsCommandHelper
+
+  describe 'regex' do
+    test_regex_against_commands <<-EOL
+      # some command
+    EOL
+  end
+
+  describe '.run' do
+    it "tests some thing" do
+    end
+  end
+end
+    FILE
+  end
+end
