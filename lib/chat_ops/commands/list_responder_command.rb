0e8720f34a061a70d1a82b6c617b5cd5fba2a039
Lex Neva <lneva@heroku.com>
2016-09-16 15:14:28 -0400

    update list and remove responder to use parse_incident

--- lib/chat_ops/commands/list_responder_command.rb
+++ lib/chat_ops/commands/list_responder_command.rb
@@ -1,11 +1,10 @@
 module ChatOps::Commands
   class ListResponderCommand < ChatOps::ChatOpsCommand
     match /incident(\s+(?<incident_id>\d+))?\s+responders/
+    parse_incident true
     help_message "h incident [#] responders - list responders for an incident"
 
-    def run(user, match)
-      incident = ChatOps.determine_incident(match['incident_id']) or return ChatOps.unknown_incident
-
+    def run(user, match, incident)
       ChatOps.message("Incident responders for incident #{incident.incident_id}: #{incident.responders.map(&:name).join(", ")}")
     end
   end
--- lib/chat_ops/commands/remove_responder_command.rb
+++ lib/chat_ops/commands/remove_responder_command.rb
@@ -1,11 +1,10 @@
 module ChatOps::Commands
   class RemoveResponderCommand < ChatOps::ChatOpsCommand
     match /remove\s+(?<who>.*)\s+from\s+incident(\s+(?<incident_id>\d+))?/
+    parse_incident true
     help_message "h remove <person> [<person>...] from incident -- <person> can be a full name or @handle"
 
-    def run(user, match)
-      incident = ChatOps.determine_incident(match['incident_id']) or return ChatOps.unknown_incident
-
+    def run(user, match, incident)
       responders = ChatOps.get_mentioned_users(match['who'])
       return ChatOps.error("Person not found.  Try using their @handle.") if responders.length == 0
 
