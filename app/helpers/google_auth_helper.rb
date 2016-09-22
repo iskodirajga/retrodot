ee9d01bde1d5cd532c6c328df56492b0f85893fc
Joshua Tobin <joshuatobin@gmail.com>
2016-09-22 12:50:20 -0400

    Remove comment

--- app/helpers/google_auth_helper.rb
+++ app/helpers/google_auth_helper.rb
@@ -5,9 +5,6 @@ module AuthHelper
     include Loggerator
 
     def initialize(client_id:, client_secret:, refresh_token:, auth_code:)
-      # In order to properly auth with the Google Drive API and still use omniauth
-      # to perform the oauth dance, we need to create an instance of UserRefreshCredentials
-      # and feed it an authorization code, along with the refresh_token if it exists in the response.
       @google_auth ||= Google::Auth::UserRefreshCredentials.new(
         client_id:     client_id,
         client_secret: client_secret,
