d8323f9f642428599840e609995e3a9e61142b0f
Lex Neva <lneva@heroku.com>
2016-09-26 15:28:09 -0400

    sort Incident.timeline_entries

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -2,7 +2,7 @@ class Incident < ActiveRecord::Base
   has_many :retrospectives
   has_many :remediations, through: :retrospectives
   belongs_to :category
-  has_many :timeline_entries
+  has_many :timeline_entries, -> { order(timestamp: :asc) }
   has_and_belongs_to_many :responders, join_table: :incidents_responders, class_name: "User"
 
   default_scope { order('incident_id DESC') }
