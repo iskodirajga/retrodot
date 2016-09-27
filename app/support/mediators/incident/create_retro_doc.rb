fef277484aaa4be4bdede35f9d848023e2908145
Joshua Tobin <joshuatobin@gmail.com>
2016-09-27 09:17:10 -0400

    more mediator

--- app/support/mediators/incident/create_retro_doc.rb
+++ app/support/mediators/incident/create_retro_doc.rb
@@ -3,7 +3,7 @@ require 'google/apis/script_v1'
 module Mediators::Incident
   class CreateRetroDoc < Mediators::Base
 
-    def initialize(auth:, id:, title:, postmortem_date: false, dev_mode: true)
+    def initialize(auth:, id:, title:, postmortem_date: false, dev_mode: false)
       @auth, @id, @title, @postmortem_date, @dev_mode = auth, id, title, postmortem_date, dev_mode
 
       @service = Google::Apis::ScriptV1::ScriptService.new
@@ -12,7 +12,6 @@ module Mediators::Incident
 
     def call
       log(fn: :call, at: :create_retro_doc)
-
       resp = @service.run_script(Config.google_script_id, execution_req)
     rescue Google::Apis::ClientError
       log_error($!, fn: "call", at: "run", id: @id)
@@ -20,7 +19,7 @@ module Mediators::Incident
     end
 
     def execution_req
-      Google::Apis::ScriptV1::ExecutionRequest.new(function: Config.google_script_function, dev_mode: @dev_mode, parameters: { id: @id, title: @title, postmortem_date: @postmortem_date })
+      Google::Apis::ScriptV1::ExecutionRequest.new(function: Config.google_script_function, dev_mode: @dev_mode, parameters: [ @id, @title, @postmortem_date ])
     end
 
   end
