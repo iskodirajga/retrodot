ad141905d602d3d90395cecce4451745a538a58c
Joshua Tobin <joshuatobin@gmail.com>
2016-10-06 17:34:59 -0400

    You've got mail

---
+++ app/mailers/notifications_mailer.rb
@@ -0,0 +1,11 @@
+class NotificationsMailer < ApplicationMailer
+  def retro_followup(incident:, sender:, to:, cc:, subject:)
+    @incident = incident
+    mail(
+      sender:  sender,
+      to:      to,
+      cc:      cc,
+      subject: subject
+    )
+  end
+end
