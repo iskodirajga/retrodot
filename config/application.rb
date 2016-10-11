9def8022f3ce1a7aa9a73c903171e1f9d2811a2c
Joshua Tobin <joshuatobin@gmail.com>
2016-10-11 13:52:06 -0400

    Fix eager load paths

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Retrodot
   class Application < Rails::Application
-#    config.autoload_paths << Rails.root.join('lib')
-#    config.autoload_paths << Rails.root.join('lib', 'chat_ops')
-    #config.eager_load_paths << Rails.root.join('lib', 'chat_ops')
-    #config.paths.add "lib/chat_ops", eager_load: true
-    config.eager_load_paths.unshift("#{config.root}/lib/chat_ops")
-    #config.eager_load_paths += %W( #{config.root}/lib/chat_ops )
+    config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('lib', 'chat_ops')
+    config.eager_load_paths += %W( #{config.root}/lib/chat_ops )
   end
 end
