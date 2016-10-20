95df7dbfd941b1ae6c092743383663474bc4f30f
Joshua Tobin <joshuatobin@gmail.com>
2016-10-20 16:23:49 -0400

    Dry it up

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,9 +7,7 @@ class MessagesController < ApplicationController
     user = User.find_by(slack_user_id: message[:user_id])
     return render_error("Slack `user_id` not found in retrodot", 403) unless user
 
-    result = ChatOps.process(user, message[:text])
-
-    result = result || "command unknown, try `/timeline help`"
+    result = ChatOps.process(user, message[:text]) || "command unknown, try `/timeline help`"
 
     render json: { text: result, response_type: "in_channel" }, status: 200
   end
